0a3fc1643c2fb558408270b5d5336d886b56b9e9
jmorena-zspace <jmorena@zspace.com>
2025-11-10 12:54:39 -0300

    updates for pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || (process.env.NODE_ENV === 'production' ? '/zc-sidepeek/' : '/'),
+  base: process.env.VITE_BASE_PATH || '/zc-sidepeek/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
